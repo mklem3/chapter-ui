0fa60629fa06247a10b1de4f5250e19e1341ea47
Matthew Klemm <matthew.klemm@comparethemarket.com.au>
2018-11-02 16:00:46 +1000

    added chapter data

--- src/mobx/chapterStore.ts
+++ src/mobx/chapterStore.ts
@@ -2,14 +2,45 @@ import { observable } from 'mobx';
 
 export interface Chapter {
   id: number;
+  users: User[];
+  date: Date;
+}
+
+export interface Issue {
+  id: number;
+  title: string;
+  description: string;
+  created: Date;
+  lastUpdated: Date;
+  completed: boolean;
+}
+
+export interface Event {
+  id: number;
+  text: string;
+  created: Date;
+  lastUpdated: Date;
+}
+
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  created: Date;
+  lastUpdated: Date;
+  deleted: Date;
+  active: boolean;
+  events: Event[];
+  issues: Issue[];
 }
 
 class ChapterStore {
   @observable
-  public chapters: Chapter[] = [{ id: 1 }, { id: 2 }];
+  public chapters: Chapter[] = [{ id: 1, users: [], date: new Date() }];
 
   public addChapter = () => {
-    this.chapters.push({ id: this.chapters.length })
+    this.chapters.push({ id: this.chapters.length + 1, users: [], date: new Date() })
   }
 }
 
